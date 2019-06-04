7b721d215823bff91355b237b1ed33ed768b53a3
Lucas Litter Mentz <lucas@mentz.dev>
2019-06-04 15:07:58 -0300

    Conteúdo base

--- WeBlock/frontend/src/views/Home.vue
+++ WeBlock/frontend/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="fluid">
+      <b-row align-h="justify">
+        <b-card
+          title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+        <!-- </b-row>
+        <b-row>-->
+        <p>oi</p>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
