2fc382a05a3d90180eab9d1fd7975c83932dbaee
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2019-06-04 22:02:05 -0300

    Esqueleto de front-end

--- WeBlock/backend/index.js
+++ WeBlock/backend/index.js
@@ -1,1 +1,2 @@
 console.log('Estou vivo!');
+
--- WeBlock/frontend/src/App.vue
+++ WeBlock/frontend/src/App.vue
@@ -1,12 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Muito Foda Isso Aqui</router-link>|
-      <router-link to="/about">About</router-link>
+    <div id="nav" v-if="!login">
+      <b-row align-h="between" align-v="center">
+        <b-col>
+          <span>
+            <h3><b>WeBlock</b></h3>
+            <p style="margin: -10px 0 4px 0;">Bloqueio de navegação</p>
+          </span>
+        </b-col>
+        <b-col>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/agendamentos">Agendamentos</router-link>
+        </b-col>
+        <b-col>
+          <b-button @click="logoff">
+            Sair
+          </b-button>
+        </b-col>
+      </b-row>
     </div>
-    <router-view/>
+    <b-container id="body" fluid>
+      <router-view/>
+    </b-container>
+    <footer>
+      <div id="footer">
+        <b>WeBlock</b> © 2019
+      </div>
+    </footer>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    logoff() {
+      alert("Desconectado :D\nTroque isso por uma função.");
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -16,8 +48,18 @@
   text-align: center;
   color: #2c3e50;
 }
+
+#body {
+  background-color: #ffffff;
+  padding: 2em;
+  /* box-shadow: 0 0 23px #cccccc; */
+  border-style: solid;
+  border-color: #2c3e50;
+  border-width: 1px 0 1px 0;
+}
+
 #nav {
-  padding: 30px;
+  padding: 1.2em;
 }
 
 #nav a {
@@ -27,5 +69,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#footer {
+  margin: 2em;
+  /* padding: 1em; */
 }
 </style>
